4610e131d3f10b1f5715971f588818ebd8ac2654
Zachary Perry <znperry45@gmail.com>
2026-02-03 13:37:53 -0400

    Set up VitePress local dev workflow

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,19 @@
+export default {
+  title: "45Homelab Hub",
+  description: "Centralized links and notes",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Videos", link: "/videos/cloud-init/" }
+    ],
+    sidebar: [
+      {
+        text: "Videos",
+        items: [
+          { text: "Cloud-Init", link: "/videos/cloud-init/" }
+        ]
+      }
+    ]
+  }
+}
+
